8676ae1fe3c4bb4661cc3f6c08ab35ecaa2f4de2
klaus <underlinemy@163.com>
2019-04-02 20:51:45 +0800

    init

---
+++ src/pages/demo/Klaus.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Klaus extends Component {
+    render () {
+        return(
+            <div className='red'>我是klaus</div>
+        )
+    }
+}
+export default Klaus;
